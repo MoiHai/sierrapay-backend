eaeba95165a58cc53c20094d843c356d02a8a6a2
Hardin Sirh Koroma <moihai.sl@gmail.com>
2026-08-02 14:00:18 +0000

    Updated database.js to read base64 file and updated gitignore

--- config/database.js
+++ config/database.js
@@ -1,23 +1,25 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const fs = require("fs");
+const path = require("path");
 
-require("dotenv").config();
+// Read the Base64 string DIRECTLY from your firebase-base64.txt file
+const base64String = fs.readFileSync(
+    path.join(__dirname, "..", "firebase-base64.txt"), 
+    "utf8"
+).trim(); // .trim() removes any hidden newlines
 
-
+// Decode Base64 string back into a JSON object
 const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
+    Buffer.from(base64String, "base64").toString("utf8")
 );
-
 
 initializeApp({
     credential: cert(serviceAccount)
 });
 
-
 const db = getFirestore();
 
-
 console.log("Firebase Firestore Connected");
-
 
 module.exports = db;
